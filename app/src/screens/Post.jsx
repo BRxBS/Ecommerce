c38a493be73c9d29679a542043a8a11e29c253eb
Bruna Santos <brunahta2025@hotmail.com>
2022-07-20 11:57:36 -0300

    2° api post

--- app/src/screens/Post.jsx
+++ app/src/screens/Post.jsx
@@ -1,11 +1,53 @@
+import React,  { useState } from "react";
 import { Header } from "../components/Header"
+import axios from "axios";
+import { useEffect } from "react";
+
+
+const api = axios.create({
+  url: "http://localhost:8000/62d18eb2128f70778a91fb7b",
+  headers: {
+    authentication: "62d18eb2128f70778a91fb7b",
+  },
+});
 
 export function Post() {
+  const [productName, setProductName] = useState("");
+  const [productDescription, setProductDescription] = useState('');
+  const [productPrice, setProductPrice] = useState('');
+  const [productQuantity, setProductQuantity] = useState("");
+  const [productImage, setProductImage] = useState('');
+
+  const [post, setPost] = useState('')
+
+
+  useEffect(() => {
+    axios.get(api).then((res) => {
+      setPost(res.data)
+    })
+  },[])
+
+  function createPost() {
+    axios.post(api, {
+      productName: { productName },
+      productDescription: { productDescription },
+      productPrice: { productPrice },
+      productQuantity: { productQuantity },
+      productImage: { productImage }
+    }).then((res) => {
+      setPost(res.data)
+    })
+  }
+
+
     return (
       <div className="block ">
         <Header />
         <div className="w-screen bg-gray-100">
-          <form action="" className="block mx-20 bg-gray-100 mt-24">
+          <form
+            onSubmit={createPost}
+            className="block mx-20 bg-gray-100 mt-24"
+          >
             <label htmlFor="" className="text-2xl font-semibold">
               Product Name
             </label>{" "}
@@ -13,6 +55,8 @@ export function Post() {
             <input
               type="text"
               className="w-64 p-1 rounded border-2 border-black"
+              onChange={(e) => setProductName(e.target.value)}
+              value={post.productName}
             />
             <br />
             <label htmlFor="" className="text-2xl font-semibold">
@@ -20,11 +64,11 @@ export function Post() {
             </label>{" "}
             <br />
             <textarea
-              name=""
-              id=""
               cols="30"
-              rows="10"
+              rows="5"
               className="w-64 p-1 rounded border-2 border-black"
+              onChange={(e) => setProductDescription(e.target.value)}
+              value={post.productDescription}
             ></textarea>
             <br />
             <label htmlFor="" className="text-2xl font-semibold">
@@ -34,6 +78,8 @@ export function Post() {
             <input
               type="text"
               className="w-64 p-1 rounded border-2 border-black"
+              onChange={(e) => setProductPrice(e.target.value)}
+              value={post.productPrice}
             />
             <br />
             <label htmlFor="" className="text-2xl font-semibold">
@@ -43,6 +89,8 @@ export function Post() {
             <input
               type="text"
               className="w-64 p-1 rounded border-2 border-black"
+              onChange={(e) => setProductQuantity(e.target.value)}
+              value={post.productQuantity}
             />
             <br />
             <label htmlFor="" className="text-2xl font-semibold">
@@ -52,12 +100,14 @@ export function Post() {
             <input
               type="text"
               className="w-64 p-1 rounded border-2 border-black"
+              onChange={(e) => setProductImage(e.target.value)}
+              value={post.productImage}
             />
             <button
               className="border p-2 w-24 m-8 absolute left-28 border-black bg-black rounded text-white "
               type="submit"
             >
-              Login
+              submit
             </button>
           </form>
         </div>
