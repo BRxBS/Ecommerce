948db7880ebb5716197d9df32863383ac33e238e
Bruna Santos <brunahta2025@hotmail.com>
2022-07-22 16:31:08 -0300

    sideBar

---
+++ app/src/components/SideBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function SideBar() {
+  <>
+    <aside className="w-1/4 bg-gray-700 p-6 border-l border-gray-600 ">
+      <div>
+        <p>Addresses</p>
+        <p>post</p>
+      </div>
+    </aside>
+  </>;
+}
