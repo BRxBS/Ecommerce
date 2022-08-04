f7d7f6b2a83fb771302d6f86053ba4ba03ba7550
Bruna Santos <brunahta2025@hotmail.com>
2022-08-04 17:20:03 -0300

    li key

--- app/src/components/SideBar.jsx
+++ app/src/components/SideBar.jsx
@@ -11,7 +11,7 @@ export function SideBar() {
     <>
       {!open ? (
         <List
-          size={38}
+          size={42}
           className="py-1 h-[5.375rem] mx-8 bg-black text-white cursor-pointer"
           onClick={() => setOpen(!open)}
         />
@@ -20,16 +20,16 @@ export function SideBar() {
           <div className="h-[5.375rem] py-1 bg-black">
             <X
               size={34}
-              className="ml-14 mt-5 md:ml-[6rem] md:mt-5 text-white cursor-pointer"
+              className="ml-12 mt-5 md:ml-[6rem] md:mt-5 text-white cursor-pointer"
               onClick={() => setOpen(!open)}
             />
           </div>
 
-          {SideBarData.map((item, index) => {
+          {SideBarData.map((item) => {
             return (
               <ul className="w-40 ">
                 <li
-                  key={index}
+                  key={item.id}
                   className="flex  w-[7rem]  md:w-[10rem] p-2 md:p-6 bg-gray-700 text-white "
                 >
                   <Link to={item.path} className="flex">
