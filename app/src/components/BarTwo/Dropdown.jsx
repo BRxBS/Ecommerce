04c805caaebb729da8bfda5712a555829a4d6fd0
Bruna Santos <brunahta2025@hotmail.com>
2022-11-04 16:33:15 -0300

    isMulti

--- app/src/components/BarTwo/Dropdown.jsx
+++ app/src/components/BarTwo/Dropdown.jsx
@@ -1,12 +1,12 @@
-import {CaretDown} from 'phosphor-react'
+import {CaretDown, X} from 'phosphor-react'
 import { useEffect } from 'react';
 import { useState } from 'react';
 import './styles.scss'
 
-export const Dropdown = ({ placeHolder, options }) => {
+export const Dropdown = ({ placeHolder, options, isMulti }) => {
 
     const [showMenu, setShowMenu] = useState(false)
-    const [selectValue, setSelectValue] = useState(null)
+    const [selectedValue, setSelectedValue] = useState(isMulti ? [] : null);
 
     useEffect(() => {
         const handler = () => setShowMenu(false)
@@ -17,29 +17,67 @@ export const Dropdown = ({ placeHolder, options }) => {
         }
     })
 
-    const handleinputClick = (e) => {
+    const handleInputClick = (e) => {
         e.stopPropagation()
         setShowMenu(!showMenu)
     }
+
     const getDisplay = () => {
-        if(selectValue) {
-            return selectValue.title
-        }
-      return placeHolder;
+      if (!selectedValue || selectedValue.length === 0) {
+        return placeHolder;
+    }
+    if (isMulti) {
+      return (
+          <div className="dropdown-tags">
+              {selectedValue.map((option) => (
+                  <div key={option.id} className="dropdown-tag-item">
+                      {option.title}
+                      <span onClick={(e) => onTagRemove(e, option)} className="dropdown-tag-close"><X size={20} /></span>
+                  </div>
+              ))}
+          </div>
+      );
+  }
+    return selectedValue.title;
     };
+
+
+    const removeOption = (option) => {
+    return selectedValue.filter((o) => o.value !== option.value);
+    };
+
+    const onTagRemove = (e, option) => {
+        e.stopPropagation();
+        setSelectedValue(removeOption(option));
+    };
+
     const onItemClick = (option) => {
-        setSelectValue(option)
-    } 
-    const isSelected = (option) => {
-        if (!selectValue) {
-            return false
+        let newValue;
+        if (isMulti) {
+            if (selectedValue.findIndex((o) => o.id === option.id) >= 0) {
+                newValue = removeOption(option);
+            } else {
+                newValue = [...selectedValue, option];
+            }
+        } else {
+            newValue = option;
         }
-        return selectValue.id === option.id
+        setSelectedValue(newValue);
+    } 
+
+    const isSelected = (option) => {
+          if (isMulti) {
+        return selectedValue.filter((o) => o.id === option.id).length > 0;
+    }
+    if (!selectedValue) {
+        return false;
+    }
+        return selectedValue.id === option.id
     } 
   
     return (
       <div className="dropdown_container">
-        <div onClick={handleinputClick} className="dropdown_input">
+        <div onMouseOver={handleInputClick} className="dropdown_input">
            {showMenu && (
              <div className='dropdown_menu'>
                 {options.map((option) => (
--- app/src/components/BarTwo/index.jsx
+++ app/src/components/BarTwo/index.jsx
@@ -7,18 +7,12 @@ import { DropdownData } from "./DropdownData";
 export function BarTwo() {
   //min-w-full top-[5.5rem] left-0 right-0 max-w-full  py-2 block md:flex  bg-gray-100 
  
-  // <button className="section">Sapatos</button>
- // <button className="section">Blusas</button>            
- // <button className="section">Calças</button>
+
   return (
     <div className="container_barTwo">
 
-
-      <Dropdown placeHolder='Select...' options={DropdownData}
+      <Dropdown isMulti placeHolder='Select...' options={DropdownData}
              />   
- 
-
-
 
       <div className="Wrapper">
         <input
