0171fee7e54ea6f939412d59105b8059e5c908a8
Bruna Santos <brunahta2025@hotmail.com>
2022-12-06 18:26:47 -0300

    minimumSignificantDigits

--- app/src/util/format.jsx
+++ app/src/util/format.jsx
@@ -1,4 +1,9 @@
 export const { format: formatPrice } = new Intl.NumberFormat('pt-br', {
   style: 'currency',
   currency: 'BRL',
+  useGrouping: true,
+  minimumSignificantDigits: 4,
+  maximumSignificantDigits:10
+
+  
 });
