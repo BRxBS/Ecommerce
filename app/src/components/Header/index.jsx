1ef2e3683d84b3e97c6c0e8df25e8e9817fa008c
Bruna Santos <brunahta2025@hotmail.com>
2022-12-15 16:24:19 -0300

    icons-cleaning

--- app/src/components/Header/index.jsx
+++ app/src/components/Header/index.jsx
@@ -21,53 +21,27 @@ export function Header() {
           <Icon className="icon" />
         </Link>
         
-        <Link to={"/"} className="small_icon" >
-          <SmallIcon  />
-        </Link>
 
 
         <div className="wrapper_icons">
-        <div className = 'search-box'>
-      <input className = "search-text" type="text" placeholder = "Pesquise o produto"/>
-     <a href="#" className = "search-btn">
-      <i className="fas fa-search">
-         <MagnifyingGlass size={36}/>
-      </i>
-
-      </a>
-    
-       </div>
 
           <Link 
-         
           to={"/cart"}>
             <ShoppingCart size={38} className=" ShoppingCart" />
           </Link>
-          {cartSize === 0 ? '' :  <p
-            className="cartLength"
-            >{cartSize}</p>}
+          {cartSize === 0 ? '' :  
+          <p className="cartLength"
+          > {cartSize} </p>}
          
-
-     { //    <Link to={"/Cart"} className="small_ShoppingCart">
-        //    <ShoppingCart size={30}  />
-        //  </Link>
-     }
 
           <Link to={"/favorite"} >
             { favActive < 1 
             ? <HeartStraight size={38} className='HeartStraight'/>
             : <HeartStraight size={38} className='HeartStraight'   weight="fill" color= "#017ff0"  /> 
-
             }
-
           </Link>
 
-        { // <Link to={"/Cart"} className='small_HeartStraight' >
-         // <HeartStraight size={30} />
-         // </Link>
-         }
-
-          <TheModal />
+      
         </div>
       </header>
     </div>
