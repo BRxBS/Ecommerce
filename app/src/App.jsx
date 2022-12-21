4bf8261c1476f6f7e8c5cf5cc09d740559c08d94
Bruna Santos <brunahta2025@hotmail.com>
2022-12-21 18:46:29 -0300

    no-userProvider

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,19 +3,17 @@ import { Router } from "./Router";
 import { Header } from "./components/Header";
 import { CartProvider }from './hooks/useCart';
 import { FavProvider } from './hooks/useFav';
-import { UserProvider } from './hooks/useUser';
+
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <FavProvider>
-          <UserProvider>
             <CartProvider>
               <Header/>
               <Router />
             </CartProvider>
-          </UserProvider>
         </FavProvider>
       </BrowserRouter>
     </>
